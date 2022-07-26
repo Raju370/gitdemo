d2c91d05b4f3117510e11594a4e102da23142ea4
Raju370 <rajuthirumani370@gmail.com>
2022-07-26 18:00:12 +0530

    DivisionApplication is created

---
+++ demoapp/src/main/java/sweetbox/DivisionApplication.java
@@ -0,0 +1,25 @@
+package sweetbox;
+
+class Division{
+	int a;
+	int b;
+	
+	public int Division(int a, int b) {
+		return a/b; 
+		
+	}
+	
+}
+
+public class DivisionApplication {
+
+	public static void main(String[] args) {
+		
+		Division div = new Division();
+		
+		System.out.println(div.Division(4, 2));
+		
+				
+	}
+
+}
